14e1c763d26333bc4d12b897c95b941aeb7ab2f3
WebAppFrameworks <wafuser@wafuser-VirtualBox.(none)>
2012-12-10 10:46:46 +0000

    Last one had some erroer with Sqlite3 gem

--- Project/Project/app/models/place.rb
+++
@@ -1,6 +0,0 @@
-class Place < ActiveRecord::Base
-  attr_accessible :city, :description, :photo_detail, :user, :image
-
-has_attached_file :image
-
-end
--- Project/Project/test/unit/helpers/flickrs_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FlickrsHelperTest < ActionView::TestCase
-end
